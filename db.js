ab29db3984a7fdee61891930cf63799eb7d6eadb
Nikhil Vikraman <nikhil.v@neoito.com>
2024-03-14 09:06:39 +0530

    feat: add update apis

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 import Database from "better-sqlite3";
 import bodySchema from "./schema.json" assert { type: "json" };
 
-const db = new Database("./todos.db");
+export const db = new Database("./todos.db");
 export const CREATETABLEQUERY = `
   CREATE TABLE IF NOT EXISTS todos (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
-  );
-`;
+    );
+    `;
+export const CREATETODOQUERY = db.prepare(
+  "INSERT OR REPLACE INTO todos (id, name) VALUES (?, ?)"
+);
+export const GETTODOSQUERY = db.prepare("SELECT * FROM todos");
 export const CREATE_EMPLOYEE_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS employees (
   uuid INTEGER PRIMARY KEY,
   employeeId TEXT NOT NULL,
@@ -45,10 +49,10 @@ export const CREATE_EMPLOYEE_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS employees
   project_name TEXT
 );`;
 
-export const CREATETODOQUERY = db.prepare(
-  "INSERT OR REPLACE INTO todos (id, name) VALUES (?, ?)"
+export const GET_EMPLOYEES_QUERY = db.prepare("SELECT * FROM employees");
+export const GET_EMPLOYEE_BY_ID_QUERY = db.prepare(
+  "SELECT * FROM employees WHERE employeeId = ?"
 );
-export const GETTODOSQUERY = db.prepare("SELECT * FROM todos");
 export const INSERT_EMPLOYEE_QUERY = db.prepare(`
   INSERT INTO employees (
     employeeId,
@@ -86,11 +90,18 @@ export const INSERT_EMPLOYEE_QUERY = db.prepare(`
     project_name
   ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 `);
-export const GET_EMPLOYEES_QUERY = db.prepare("SELECT * FROM employees");
-export const GET_EMPLOYEE_BY_ID_QUERY = db.prepare(
-  "SELECT * FROM employees WHERE employeeId = ?"
+
+export const UPDATE_EMPLOYEE_PERSONAL_QUERY = db.prepare(
+  "UPDATE employees SET name = ?, dob = ?, age = ?, permanent_address = ?, present_address = ?, city = ?, company_mailid = ?, personal_mailid = ?, gender = ?, contact = ?, emergency_contact = ?, blood_group = ? WHERE employeeId = ?"
 );
-db.exec(CREATETABLEQUERY);
+export const UPDATE_EMPLOYEE_PROFESSIONAL_QUERY = db.prepare(
+  "UPDATE employees SET career_start_date = ?, neoito_start_date = ?, qualification = ?, total_years = ?, neoito_years = ?, ctc_exclude_gratuity = ?, effective_gratuity = ?, bonus = ?, designation = ?, department = ?, pm = ?, reporting = ?, additional_reporting = ?, office_location = ?, office_status = ?, project_name = ? WHERE employeeId = ?"
+);
+export const UPDATE_EMPLOYEE_SKILLS_QUERY = db.prepare(
+  "UPDATE employees SET front_end_skills = ?, back_end_skills = ?, db_skills = ?, cloud_skills = ? WHERE employeeId = ?"
+);
+
+// db.exec(CREATETABLEQUERY);
 db.exec(CREATE_EMPLOYEE_TABLE_QUERY);
 
 export const schema = {
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import {
   GETTODOSQUERY,
   INSERT_EMPLOYEE_QUERY,
   schema,
+  UPDATE_EMPLOYEE_PERSONAL_QUERY,
+  UPDATE_EMPLOYEE_PROFESSIONAL_QUERY,
+  UPDATE_EMPLOYEE_SKILLS_QUERY,
 } from "./db.js";
 
 const fastify = Fastify({
@@ -91,8 +94,6 @@ fastify.post("/addemployee", { schema }, async (request, reply) => {
       gender,
       project_name,
     } = request.body;
-
-    // Execute the prepared statement to insert employee data
     const result = INSERT_EMPLOYEE_QUERY.run(
       employeeId,
       name,
@@ -128,7 +129,6 @@ fastify.post("/addemployee", { schema }, async (request, reply) => {
       gender,
       project_name
     );
-
     return {
       success: true,
       message: "Employee data successfully added to the database",
@@ -164,6 +164,147 @@ fastify.get("/getemployee/:employeeId", async (request, reply) => {
     reply.status(500).send({ error: "Internal Server Error" });
   }
 });
+
+fastify.patch(
+  "/updateemployee/:employeeId/personal",
+  { schema },
+  async (request, reply) => {
+    try {
+      const { employeeId } = request.params;
+      const {
+        name,
+        dob,
+        age,
+        permanent_address,
+        present_address,
+        city,
+        company_mailid,
+        personal_mailid,
+        gender,
+        contact,
+        emergency_contact,
+        blood_group,
+      } = request.body;
+      const existingEmployee = GET_EMPLOYEE_BY_ID_QUERY.get(employeeId);
+      if (!existingEmployee) {
+        reply.status(404).send({ error: "Employee not found" });
+        return;
+      }
+      UPDATE_EMPLOYEE_PERSONAL_QUERY.run(
+        name,
+        dob,
+        age,
+        permanent_address,
+        present_address,
+        city,
+        company_mailid,
+        personal_mailid,
+        gender,
+        contact,
+        emergency_contact,
+        blood_group,
+        employeeId
+      );
+      return {
+        success: true,
+        message: "Employee Personal details updated successfully",
+      };
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ error: "Internal Server Error" });
+    }
+  }
+);
+
+fastify.patch(
+  "/updateemployee/:employeeId/professional",
+  { schema },
+  async (request, reply) => {
+    try {
+      const { employeeId } = request.params;
+      const {
+        career_start_date,
+        neoito_start_date,
+        qualification,
+        total_years,
+        neoito_years,
+        ctc_exclude_gratuity,
+        effective_gratuity,
+        bonus,
+        designation,
+        department,
+        pm,
+        reporting,
+        additional_reporting,
+        office_location,
+        office_status,
+        project_name,
+      } = request.body;
+      const existingEmployee = GET_EMPLOYEE_BY_ID_QUERY.get(employeeId);
+      if (!existingEmployee) {
+        reply.status(404).send({ error: "Employee not found" });
+        return;
+      }
+      UPDATE_EMPLOYEE_PROFESSIONAL_QUERY.run(
+        career_start_date,
+        neoito_start_date,
+        qualification,
+        total_years,
+        neoito_years,
+        ctc_exclude_gratuity,
+        effective_gratuity,
+        bonus,
+        designation,
+        department,
+        pm,
+        reporting,
+        additional_reporting,
+        office_location,
+        office_status,
+        project_name,
+        employeeId
+      );
+      return {
+        success: true,
+        message: "Employee Professional details updated successfully",
+      };
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ error: "Internal Server Error" });
+    }
+  }
+);
+
+fastify.patch(
+  "/updateemployee/:employeeId/skills",
+  { schema },
+  async (request, reply) => {
+    try {
+      const { employeeId } = request.params;
+      const { front_end_skills, back_end_skills, db_skills, cloud_skills } =
+        request.body;
+      const existingEmployee = GET_EMPLOYEE_BY_ID_QUERY.get(employeeId);
+      if (!existingEmployee) {
+        reply.status(404).send({ error: "Employee not found" });
+        return;
+      }
+      UPDATE_EMPLOYEE_SKILLS_QUERY.run(
+        front_end_skills,
+        back_end_skills,
+        db_skills,
+        cloud_skills,
+        employeeId
+      );
+      return {
+        success: true,
+        message: "Employee Skills details updated successfully",
+      };
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ error: "Internal Server Error" });
+    }
+  }
+);
 
 try {
   await fastify.listen({ port: 3000 });
